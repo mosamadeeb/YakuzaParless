a5d664ca3a2a1bd2468b96f62629a62dd94d4a8d
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2021-04-23 15:27:38 +0300

    Add Kiwami 2 support

--- source/Parless.cpp
+++ source/Parless.cpp
@@ -450,7 +450,45 @@ void OnInitializeHook()
                 break;
             }
             case Game::YakuzaKiwami2:
+            {
+                // Hook inside the method that calculates a string's length to add 0x20 bytes to the length
+                // This is needed to prevent undefined behavior when the modified path is longer than the memory allocated to it
+                auto stringLenAddr = get_pattern("8B C7 3B D8 77 4E", -0x1C);
+
+                Trampoline* trampolineStringLen = Trampoline::MakeTrampoline(stringLenAddr);
+
+                const uint8_t spacePayload[] = {
+                    0x48, 0x8B, 0x09, // mov rcx, qword ptr ds:[rcx]
+                    0x48, 0x83, 0xC3, 0x20, // add rbx, 0x20
+                    0x48, 0x8B, 0xC1, // mov rax, rcx
+                    0x48, 0xC1, 0xE8, 0x2C, // shr rax, 0x2C
+                    0xE9, 0x0, 0x0, 0x0, 0x0 // jmp stringLenAddr+0xA
+                };
+
+                std::byte* space = trampolineStringLen->RawSpace(sizeof(spacePayload));
+                memcpy(space, spacePayload, sizeof(spacePayload));
+
+                VP::WriteOffsetValue(space + 3 + 4 + 3 + 4 + 1, reinterpret_cast<intptr_t>(stringLenAddr) + 0xA);
+
+                const uint8_t funcPayload[] = {
+                    0x48, 0x8B, 0xDB, // mov rbx, rbx
+                    0x90, 0x90, // nop
+                    0xE9, 0x0, 0x0, 0x0, 0x0 // jmp space2
+                };
+
+                memcpy(stringLenAddr, funcPayload, sizeof(funcPayload));
+
+                VP::InjectHook(reinterpret_cast<intptr_t>(stringLenAddr) + 5, space, PATCH_JUMP);
+
+                // Hook the AddFileEntry method to get each filepath that is loaded in the game
+
+                // Unlike Y6, K2 has a good place to hook the call instead of hooking the first instruction of the function
+                renameFilePathsFunc = get_pattern("4C 8D 44 24 20 48 8B D3 48 8B CF", -5);
+                VP::ReadCall(renameFilePathsFunc, orgY6AddFileEntry);
+
+                VP::InjectHook(renameFilePathsFunc, trampoline->Jump(Y6AddFileEntry));
                 break;
+            }
             case Game::YakuzaLikeADragon:
                 break;
             case Game::Unsupported:
